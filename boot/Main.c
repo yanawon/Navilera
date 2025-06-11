a7253f0e42f16e523a9c10cb1c0da6c99e22017a
yanawon <sewon4109@gmail.com>
2025-06-11 15:00:54 +0900

    messeging

--- boot/Main.c
+++ boot/Main.c
@@ -106,14 +106,33 @@ void User_task0(void)
         uint32_t local = 0;
         debug_printf("User Task #0 SP=0x%x\n", &local);
         
+        uint8_t cmdBuf[16];
+        uint32_t cmdBufIdx = 0;
+        uint8_t uartch = 0;
+        
         while(true)
         {
                 KernelEventFlag_t handle_event = Kernel_wait_events(KernelEventFlag_UartIn|KernelEventFlag_CmdOut);
                 switch(handle_event)
                 {
                 case KernelEventFlag_UartIn:
-                        debug_printf("\nEvent handled by Task0\n");
-                        Kernel_send_events(KernelEventFlag_CmdIn);
+                        Kernel_recv_msg(KernelMsgQ_Task0, &uartch, 1);
+                        if (uartch == '\r')
+                        {
+                                cmdBuf[cmdBufIdx] = '\0';
+                                
+                                Kernel_send_msg(KernelMsgQ_Task1, &cmdBufIdx, 1);
+                                Kernel_send_msg(KernelMsgQ_Task1, cmdBuf, cmdBufIdx);
+                                Kernel_send_events(KernelEventFlag_CmdIn);
+                                
+                                cmdBufIdx = 0;
+                        }
+                        else
+                        {
+                                cmdBuf[cmdBufIdx] = uartch;
+                                cmdBufIdx++;
+                                cmdBufIdx %= 16;
+                        }
                         break;
                 case KernelEventFlag_CmdOut:
                         debug_printf("\nCmdOut Event by Task0\n");
@@ -126,7 +145,11 @@ void User_task0(void)
 void User_task1(void)
 {
         uint32_t local = 0;
+        
         debug_printf("User Task #1 SP=0x%x\n", &local);        
+        
+        uint8_t cmdlen = 0;
+        uint8_t cmd[16] = {0};
         
         while(true)
         {
@@ -134,7 +157,10 @@ void User_task1(void)
                 switch(handle_event)
                 {
                 case KernelEventFlag_CmdIn:
-                        debug_printf("\nEvent handled by Task1\n");
+                        memclr(cmd, 16);
+                        Kernel_recv_msg(KernelMsgQ_Task1, &cmdlen, 1);
+                        Kernel_recv_msg(KernelMsgQ_Task1, cmd, cmdlen);
+                        debug_printf("\nRecv Cmd: %s\n", cmd);
                         
                         break;
                 }
